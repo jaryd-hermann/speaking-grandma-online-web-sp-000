f99ea75048d1df9bb1daba0871926e3394aedffc
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-08 12:37:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,12 +7,10 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
   return "NO, NOT SINCE 1938!"
-
 elsif phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   return "I LOVE YOU TOO PUMPKIN!"
 end
-
 end
 
 
