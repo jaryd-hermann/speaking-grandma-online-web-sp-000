e127209d440780f872882cfe11205744e29c7f21
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-08 12:33:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ elsif phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   return "I LOVE YOU TOO PUMPKIN!"
 end
-  
+
 end
 
 
@@ -19,7 +19,7 @@ end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
